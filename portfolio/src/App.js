bf86c9021e0425a08c843b8a98af018e4dab3d0a
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-09-07 12:01:40 -0700

    working

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
 import  Followme from './Components/Followme';
+import Aboutm from './Components/Aboutm';
 
 function App() {
   return (
     <>
       <Header />
+      <Aboutm/>
       {/* <Followme /> */}
       
     </>

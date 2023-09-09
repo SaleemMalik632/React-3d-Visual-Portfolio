736291e99072ab98bb3ec99c39259d2e0f3a42ac
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-09-09 01:54:02 -0700

    working

--- portfolio/src/Components/Myskils.js
+++ portfolio/src/Components/Myskils.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {  ToggleButtonGroup } from '@mui/material';
+import { ToggleButtonGroup } from '@mui/material';
 import MuiToggleButton from '@mui/material/ToggleButton';
 import { styled } from '@mui/material/styles';
 
@@ -15,31 +15,37 @@ const ToggleButtonExample = () => {
             color: 'white',
             backgroundColor: selectedColor,
             fontWeight: 'bold'
-            
+
         }
     }))
 
     return (
-        <div className='skill'>
-            <ToggleButtonGroup
-                value={selected}
-                exclusive
-                onChange={handleAlignment}
-            >
-                <ToggleButton selectedColor="green" value="button1" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white' ,paddingLeft:'20px',paddingRight:'20px'}}>
-                    Web
-                </ToggleButton>
-                <ToggleButton selectedColor="green" value="button2" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white', paddingLeft:'20px',paddingRight:'20px' }}>
-                   others
-                </ToggleButton>
-                <ToggleButton selectedColor="green" value="button3" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white' , paddingLeft:'20px',paddingRight:'20px'}}>
-                    Programming
-                </ToggleButton>
-                <ToggleButton selectedColor="green" value="button4" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white' , paddingLeft:'20px',paddingRight:'20px'}}>
-                    Tools
-                </ToggleButton>
-            </ToggleButtonGroup>
-        </div>
+        <>
+            <div className='skill'>
+                <ToggleButtonGroup
+                    value={selected}
+                    exclusive
+                    onChange={handleAlignment}
+                >
+                    <ToggleButton selectedColor="green" value="Web" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white', paddingLeft: '20px', paddingRight: '20px' }}>
+                        Web
+                    </ToggleButton>
+                    <ToggleButton selectedColor="green" value="others" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white', paddingLeft: '20px', paddingRight: '20px' }}>
+                        others
+                    </ToggleButton>
+                    <ToggleButton selectedColor="green" value="Programming" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white', paddingLeft: '20px', paddingRight: '20px' }}>
+                        Programming
+                    </ToggleButton>
+                    <ToggleButton selectedColor="green" value="Tools" style={{ margin: '5px', border: '2px solid green', borderRadius: '5px', color: 'white', paddingLeft: '20px', paddingRight: '20px' }}>
+                        Tools
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            </div>
+            {selected === 'Web' && 'web page'}
+            {selected === 'others' && 'others page'}
+            {selected === 'Programming' && 'Programming page'}
+            {selected === 'Tools' && 'Tools page'}
+        </>
     );
 };
 
